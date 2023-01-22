1210635cf7724a4eea6f84ccba78f37c2b1e8eae
DeepankRx <deepankpushpad10@gmail.com>
2023-01-23 00:03:40 +0530

    signup/login api integrated

--- src/components/UI/Button.jsx
+++ src/components/UI/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Button = ({children,type,classname,onClick}) => {
   return (
-    <div type={type} onClick={onClick} className={`w-[100%] bg-black text-white  font-bold flex justify-center p-2 hover:cursor-pointer hover:bg-opacity-90 ${classname}`}>{children}</div>
+    <button type={type} onClick={onClick} className={`w-[100%] bg-black text-white  font-bold flex justify-center p-2 hover:cursor-pointer hover:bg-opacity-90 ${classname}`}>{children}</button>
   )
 }
 
