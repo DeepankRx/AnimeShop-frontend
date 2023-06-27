2e20f072c80b6ed8644022770bb298cf3d8f9b41
DeepankRx <deepankpushpad10@gmail.com>
2023-06-27 20:06:09 +0530

    sitemap react-snap

--- src/index.js
+++ src/index.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { AuthContextProvider } from './store/AuthContext';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <Provider store={store}>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
-  </Provider>
-);
+const rootElement = document.getElementById('root');
+
+// If server-rendered markup exists, use hydrate
+if (rootElement.hasChildNodes()) {
+  ReactDOM.hydrate(
+    <Provider store={store}>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </Provider>,
+    rootElement
+  );
+} else {
+  // Otherwise, use render
+  ReactDOM.render(
+    <Provider store={store}>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </Provider>,
+    rootElement
+  );
+}
